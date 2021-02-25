55587fd2850c5008ff0bace104f1686adb1bc32c
Snir Yacoby <yacobysn@gmail.com>
2021-02-25 06:20:39 +0200

    End of section 4

--- src/app/posts/posts.service.ts
+++ src/app/posts/posts.service.ts
@@ -2,6 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Subject } from 'rxjs';
 import { Post } from './post.model';
+import { map } from 'rxjs/operators'
 
 @Injectable({
   providedIn: 'root'
@@ -13,9 +14,18 @@ export class PostsService {
   constructor(private http: HttpClient) { }
 
   getPosts() {
-    this.http.get<{message: string, posts: Post[]}>('http://localhost:3000/api/posts')
-      .subscribe((response) => {
-        this.posts = response.posts;
+    this.http.get<{ message: string, posts: any }>('http://localhost:3000/api/posts')
+      .pipe(map((postData) => {
+        return postData.posts.map(post => {
+          return {
+            title: post.title,
+            content: post.content,
+            id: post._id,
+          }
+        })
+      }))
+      .subscribe((posts) => {
+        this.posts = posts;
         this.postsUpdated.next([...this.posts]);
       });
   }
@@ -24,12 +34,20 @@ export class PostsService {
     return this.postsUpdated.asObservable();
   }
 
-  addPost(title:string, content: string) {
-    const post: Post = {id: null, title: title, content: content};
-    this.http.post<{message: string}>('http://localhost:3000/api/posts', post)
+  addPost(title: string, content: string) {
+    const post: Post = { id: null, title: title, content: content };
+    this.http.post<{ message: string, postId: string }>('http://localhost:3000/api/posts', post)
       .subscribe((response) => {
-        console.log(response.message);
+        post.id = response.postId;
         this.posts.push(post);
+        this.postsUpdated.next([...this.posts]);
+      });
+  }
+
+  deletePost(postId: string) {
+    this.http.delete('http://localhost:3000/api/posts/' + postId)
+      .subscribe(() => {
+        this.posts = this.posts.filter(post => post.id !== postId);
         this.postsUpdated.next([...this.posts]);
       });
   }
